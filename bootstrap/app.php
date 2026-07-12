291acb84fad4dd0edb37c68d2654829db40ac2ea
AYMANEELATRACHE <elatrachayman48@gmail.com>
2026-07-12 18:17:26 +0100

    fix: trust all proxies to resolve mixed content errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
